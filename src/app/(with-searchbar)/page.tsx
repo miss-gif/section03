3180da1b53f2e394030bdb8cc6407100ea866a1a
pc <svx327@naver.com>
2025-03-25 18:57:56 +0900

    Add: 앱라우터의 데이터 패칭

--- src/app/(with-searchbar)/page.tsx
+++ src/app/(with-searchbar)/page.tsx
@@ -1,21 +1,51 @@
 import BookItem from "@/components/book-item";
+import { BookData } from "@/types";
 import style from "./page.module.css";
-import books from "@/mock/books.json";
+
+const AllBooks = async () => {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`);
+  if (!res.ok) {
+    return <div>오류가 발생했습니다.</div>;
+  }
+  const allBooks: BookData[] = await res.json();
+
+  return (
+    <div>
+      {allBooks.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </div>
+  );
+};
+
+const RecoBooks = async () => {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`
+  );
+  if (!res.ok) {
+    return <div>오류가 발생했습니다.</div>;
+  }
+  const recoBooks: BookData[] = await res.json();
+
+  return (
+    <div>
+      {recoBooks.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </div>
+  );
+};
 
 export default function Home() {
   return (
     <div className={style.container}>
       <section>
         <h3>지금 추천하는 도서</h3>
-        {books.map((book) => (
-          <BookItem key={book.id} {...book} />
-        ))}
+        <RecoBooks />
       </section>
       <section>
         <h3>등록된 모든 도서</h3>
-        {books.map((book) => (
-          <BookItem key={book.id} {...book} />
-        ))}
+        <AllBooks />
       </section>
     </div>
   );
--- src/app/(with-searchbar)/search/page.tsx
+++ src/app/(with-searchbar)/search/page.tsx
@@ -1,13 +1,27 @@
-import books from "@/mock/books.json";
 import BookItem from "@/components/book-item";
+import { BookData } from "@/types";
 
-export default function Page({
+export default async function Page({
   searchParams,
 }: {
   searchParams: Promise<{
     q?: string;
   }>;
 }) {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${
+      (
+        await searchParams
+      ).q
+    }`
+  );
+
+  if (!res.ok) {
+    return <div>오류가 발생했습니다.</div>;
+  }
+
+  const books: BookData[] = await res.json();
+
   return (
     <div>
       {books.map((book) => (
