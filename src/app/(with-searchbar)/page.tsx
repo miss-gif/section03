82373dd93258916df9ce2042d792045367a329b3
pc <svx327@naver.com>
2025-03-25 23:57:15 +0900

    Add: fetch 요청에 캐시 및 재검증 설정 추가

--- src/app/(with-searchbar)/page.tsx
+++ src/app/(with-searchbar)/page.tsx
@@ -3,7 +3,9 @@ import { BookData } from "@/types";
 import style from "./page.module.css";
 
 const AllBooks = async () => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`);
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`, {
+    cache: "no-store",
+  });
   if (!res.ok) {
     return <div>오류가 발생했습니다.</div>;
   }
@@ -20,7 +22,12 @@ const AllBooks = async () => {
 
 const RecoBooks = async () => {
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`,
+    {
+      next: {
+        revalidate: 3,
+      },
+    }
   );
   if (!res.ok) {
     return <div>오류가 발생했습니다.</div>;
