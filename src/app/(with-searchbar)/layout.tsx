d2c22ce94aa405550dfe32e551078ae48380b997
pc <svx327@naver.com>
2025-03-26 05:32:13 +0900

    Add: Searchbar를 Suspense로 감싸 로딩 상태 처리

--- src/app/(with-searchbar)/layout.tsx
+++ src/app/(with-searchbar)/layout.tsx
@@ -4,7 +4,9 @@ import Searchbar from "../../components/searchbar";
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <div>
-      <Searchbar />
+      <Suspense fallback={<div>로딩 중...</div>}>
+        <Searchbar />
+      </Suspense>
       {children}
     </div>
   );
