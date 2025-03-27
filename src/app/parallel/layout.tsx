1fe89569620157dda573a121d11f701be314c077
pc <svx327@naver.com>
2025-03-28 04:31:40 +0900

    Add: 패럴렐 라우트 적용

---
+++ src/app/parallel/layout.tsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+
+export default function Layout({
+  children,
+  sidebar,
+  feed,
+}: {
+  children: React.ReactNode;
+  sidebar: React.ReactNode;
+  feed: React.ReactNode;
+}) {
+  return (
+    <>
+    <header className="flex gap-4 p-4 bg-gray-200">
+      <Link href={"/parallel"}>paralled</Link>
+      <Link href={"/parallel/setting"}>paralled/setting</Link>
+    </header>
+      <section>
+        {sidebar}
+        {feed}
+        {children}
+      </section>
+    </>
+  );
+}
